13e3a2455758c2c6ca966ec0dfdcd51ea61ba948
ling <tobeabetterman_dl@163.com>
2023-02-09 21:46:56 +0800

    feat: 购买和恢复购买

--- Swift_Sudoku_Solver/Swift_Sudoku_Solver/Classes/Purchased/PurchaseViewController.swift
+++ Swift_Sudoku_Solver/Swift_Sudoku_Solver/Classes/Purchased/PurchaseViewController.swift
@@ -12,8 +12,8 @@ import StoreKit
 
 class PurchaseViewController: UIViewController {
     
-    var purchaseOnceBtn = UIButton.init()
-    var becomeVipBtn = UIButton.init()
+//    var purchaseOnceBtn = UIButton.init()
+    var purchaseVipBtn = UIButton.init()
     var restoreBtn = UIButton.init()
     
     override func viewDidLoad() {
@@ -31,19 +31,40 @@ class PurchaseViewController: UIViewController {
         var backgroundImage = UIImage(named: "iphonetall-background-x13")!
         backgroundImage = backgroundImage.imageResize(sizeChange: CGSize.init(width: view.size.width, height: view.size.height))
         view.backgroundColor = UIColor(patternImage: backgroundImage)
-    }
-    
-    func createBtnWith(title: String ,target: Any?, action:Selector) -> UIButton {
-        let btn = UIButton.init(title: title, target: target, action: action)
-        return btn
-    }
-    
-    @objc func purchaseOnce() -> Void {
+
+        purchaseVipBtn = DLFunctionButton(title: "Purchase VIP".localizedString(), target: self, action: #selector(purchaseVip))
         
+        restoreBtn = DLFunctionButton(title: "Restore".localizedString(), target: self, action: #selector(restore))
+        
+        view.addSubview(purchaseVipBtn)
+        view.addSubview(restoreBtn)
+        
+        let y_offset = 60.0
+        purchaseVipBtn.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().offset(-y_offset)
+            make.size.equalTo(CGSize(width: 150, height: 50))
+            make.centerX.equalToSuperview()
+        }
+        
+        restoreBtn.snp.makeConstraints { make in
+            make.centerY.equalToSuperview().offset(y_offset)
+            make.size.equalTo(CGSize(width: 150, height: 50))
+            make.centerX.equalToSuperview()
+        }
     }
+    
+//    func createBtnWith(title: String ,target: Any?, action:Selector) -> UIButton {
+//        let btn = UIButton.init(title: title, target: target, action: action)
+//        btn.frame = CGRectMake(0, 0, 150, 50)
+//        return btn
+//    }
+    
+//    @objc func purchaseOnce() -> Void {
+//
+//    }
     
     @objc func purchaseVip() -> Void {
-        
+        print("purchaseVip")
     }
     
     @objc func restore() -> Void {
